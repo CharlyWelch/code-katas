5c546e872d4e33619a0184d111392d48c37e958e
CharlyWelch <blwbiology@gmail.com>
2018-01-30 11:31:59 -0800

    install pytest

--- test_descending.py
+++ test_descending.py
@@ -1,3 +1,11 @@
-test.assert_equals(Descending_Order(0), 0)
-test.assert_equals(Descending_Order(15), 51)
-test.assert_equals(Descending_Order(123456789), 987654321)
+from descending import Descending_Order
+import pytest
+import unittest
+
+class TestSum(unittest.TestCase):
+    def test_1(self):
+        self.assertEqual(Descending_Order(0), 0)
+    def test_2(self):
+        self.assertEqual(Descending_Order(15), 51)
+    def test_3(self):
+        self.assertEqual(Descending_Order(123456789), 987654321)
